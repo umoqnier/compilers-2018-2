a564983105292d8c5bf311fe64c5ab4a4a8abd3e
Diego Alberto Barriga Martínez <umoqnier@gmail.com>
2018-03-22 23:13:38 -0600

    Version enviada a la plataforma. Faltan tolerancia a errores

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 # usr/bin/bash
 
-# Borrando archivos
-rm -f lex.yy.c analizador_lexico.o
-
 # Llamada a flex
 flex analizador_lexico.l
 
@@ -11,6 +8,3 @@ gcc -lfl lex.yy.c -o analizador_lexico.o
 
 # Ejecución del binario con archivo a analizar como argumento
 ./analizador_lexico.o example.c
-
-# Impresión del archivo de salida
-#cat salida.txt
