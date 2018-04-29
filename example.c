0427782f258764c65c73d14e039f8d9683047323
Diego Alberto Barriga Martínez <umoqnier@gmail.com>
2018-04-29 14:53:25 -0500

    Corrección del error lineas en blanco antes de palabras reservadas.

--- example.c
+++ example.c
@@ -1,17 +1,25 @@
 #undef
+
 #define B "0"
+
 #ifdef B
+
 #if B>0
+
 #define PI "3.14159" ()
 main() {
   printf("Programa con B>0\n");
+
 #undef PI
   float pi = 3.14159;
 } //fin del primer programa
+
 #elif B<0
+
 main() {
   printf("Programa con B<0\n");
 } // fin del segundo programa
+
 #else
 main() {
   printf("Programa con B==0\n");
