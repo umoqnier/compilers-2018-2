4fac145c07e80d15995cb3a5250a27228a4f89ce
Diego Alberto Barriga Martínez <umoqnier@gmail.com>
2018-05-02 22:48:46 -0500

    Agregando la cadena de atomos.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 # usr/bin/bash
 
 # Llamada a flex
-flex analizador_lexico.l
+flex analizador_lexico_sintactico.l
 
 # Llamada al compilador gcc
-gcc -lfl lex.yy.c -o analizador_lexico.o
+gcc -lfl lex.yy.c -o analizador_lexico_sintactico.o
 
 # Ejecución del binario con archivo a analizar como argumento
-./analizador_lexico.o example.c
+./analizador_lexico_sintactico.o example.c
